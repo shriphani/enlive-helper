256398baf8b3b36786bbe83b33fb651b61c02a96
Shriphani Palakodety <shriphanip@gmail.com>
2014-05-15 23:47:21 -0400

    fixed deps

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[enlive "1.1.5"]
+  :dependencies [[clj-http "0.9.1"]
+                 [enlive "1.1.5"]
                  [net.sourceforge.htmlcleaner/htmlcleaner "2.8"]
                  [org.clojure/clojure "1.5.1"]])

1b75715269a97eeb892f76998554ecea994734c0
Shriphani Palakodety <shriphanip@gmail.com>
2014-05-16 01:47:46 -0400

    signing info

--- project.clj
+++ project.clj
@@ -6,4 +6,6 @@
   :dependencies [[clj-http "0.9.1"]
                  [enlive "1.1.5"]
                  [net.sourceforge.htmlcleaner/htmlcleaner "2.8"]
-                 [org.clojure/clojure "1.5.1"]])
+                 [org.clojure/clojure "1.5.1"]]
+  :signing {:gpg-key "02CF1D9A"}
+  :deploy-repositories [["clojars" {:creds :gpg}]])
